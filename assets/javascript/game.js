1adaf7bda7ac494e8b7fe0eeb833952151951026
Orlando Carnate <orlando.carnate@gmail.com>
2019-02-10 16:13:20 -0600

    Convert to mobile with keypad

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -15,6 +15,11 @@ var playInProgress = false;
 // sound variables
 var soundPlay = document.createElement('audio');
 soundPlay.volume = 0.25;
+// jQuery Audio
+// var soundPlay = $("<audio/>");
+// $("body").append(soundPlay);
+// soundPlay.attr("volume", 0.25);
+
 
 // ELEMENT VARIABLES
 var feedbackID = document.getElementById("feedback");
@@ -25,15 +30,15 @@ var fillInTheBlankID = document.getElementById("fillInTheBlank");
 var alreadyGuessedID = document.getElementById("alreadyGuessed");
 
 // word list array
-var wordList = 
+var wordList =
     ['html', 'css', 'alphabet', 'prestige', 'javascript',
-    'jquery', 'bootstrap', 'flexbox', 'coding', 'developer',
-    'programmer'];
+        'jquery', 'bootstrap', 'flexbox', 'coding', 'developer',
+        'programmer'];
 
 // WORD GAME OBJECT
 var wordGame = {
     // Pick random word from wordList
-    randomize: function() {
+    randomize: function () {
         // reset already guessed letters
         alreadyGuessedArray = [];
         alreadyGuessedID.textContent = "NONE";
@@ -48,7 +53,7 @@ var wordGame = {
 
         // create a variable with underlines and spaces.
         fillInTheBlank = '';
-        for (i=0; i<guessCount; i++) {
+        for (i = 0; i < guessCount; i++) {
             fillInTheBlank += '_';
         }
         fillInTheBlank = fillInTheBlank.split('').join(' ');
@@ -58,7 +63,7 @@ var wordGame = {
     },
 
     // Play a sound
-    playSound: function(melody) {
+    playSound: function (melody) {
         switch (melody) {
             case "melody":
                 randomNum = Math.floor(Math.random() * 4 + 1);
@@ -76,19 +81,25 @@ var wordGame = {
         }
         soundPlay.src = soundFile;
         soundPlay.play();
-        
+
     },
 
     // Play Letter Sound function
-    playLetter: function(letter) {
+    playLetter: function (letter) {
+        // callback another audio after first audio is played
         var letterSound = "assets/sounds/Alphabet/" + letter.toUpperCase() + ".wav";
         console.log(letterSound);
         soundPlay.src = letterSound;
         soundPlay.play();
+
+        // callback loops forever, will comment out for now.
+        // soundPlay.onended = function() {
+        //     wordGame.playSound("melody");
+        // }
     },
 
     // start game picks a new word but keeps score.
-    playGame: function() {
+    playGame: function () {
         // call method within the object
         this.playSound("melody");
         var guessCount = 0;
@@ -100,7 +111,7 @@ var wordGame = {
     },
 
     // reset game and score
-    resetGame: function() {
+    resetGame: function () {
         this.playSound("melody");
         winCount = 0;
         loseCount = 0;
@@ -115,14 +126,14 @@ var wordGame = {
     },
 
     // PUSH char to array function
-    pushAlreadyGuessed: function(letter) {
+    pushAlreadyGuessed: function (letter) {
         console.log("You chose: " + letter);
         alreadyGuessedArray.push(letter);
         alreadyGuessedID.textContent = alreadyGuessedArray.join(', ');
     },
 
     // validate keypress
-    validateKey: function(code) {
+    validateKey: function (code) {
         if ((code >= 65 && code <= 90) || (code >= 97 && code <= 122)) {
             return true;
         }
@@ -132,9 +143,9 @@ var wordGame = {
     },
 
     // does pressed key match any letter in the random word?
-    findMatch: function(userkey) {
+    findMatch: function (userkey) {
         // run a for loop to compare each letter and set charExists to True
-        for (i=0; i < randomWord.length; i++) {
+        for (i = 0; i < randomWord.length; i++) {
             if (userkey === randomWord.charAt(i)) {
                 blankArray = fillInTheBlank.split(' ');
                 blankArray[i] = userkey;
@@ -145,7 +156,7 @@ var wordGame = {
 
                 //play melody sound after playing letter sound
                 this.playLetter(userKey);
-                this.playLetter
+                // this.playLetter
                 // Player WINS if joinedBlank is equal to wordArray
                 console.log(revealedLetters, randomWord);
                 if (revealedLetters === randomWord) {
@@ -157,24 +168,24 @@ var wordGame = {
                 }
             }
         }
-            
+
         // if no match then reduce # of guesses by 1
         if (!charExists) {
             guessCount--;
             guessCountID.textContent = guessCount;
             this.playLetter(userKey);
             if (guessCount === 0) {
-            feedbackID.textContent = "YOU LOSE - PLAY AGAIN?";
-            this.playSound("lose");
-            loseCount++;
-            lossesID.textContent = loseCount;
+                feedbackID.textContent = "YOU LOSE - PLAY AGAIN?";
+                this.playSound("lose");
+                loseCount++;
+                lossesID.textContent = loseCount;
 
-            // display the word
-            fillInTheBlankID.textContent = randomWord.split('').join(' ');
+                // display the word
+                fillInTheBlankID.textContent = randomWord.split('').join(' ');
 
-            // stop player key input
-            playInProgress = false; 
-            } 
+                // stop player key input
+                playInProgress = false;
+            }
 
         }
 
@@ -186,17 +197,36 @@ var wordGame = {
 }
 
 // Event Listener for Play button
-document.getElementById("playButton").addEventListener("click", function(){
+document.getElementById("playButton").addEventListener("click", function () {
     wordGame.playGame();
 });
 
 // Event Listener for Reset button
-document.getElementById("resetButton").addEventListener("click", function(){
+document.getElementById("resetButton").addEventListener("click", function () {
     wordGame.resetGame();
 });
 
+// Event Listener for on-screen buttons
+$(document).on("click", ".letter", function () {
+    // alert("Pressed");
+    userKey = $(this).attr("value");
+    if (playInProgress) {
+
+        // continue of keypress is a valid letter
+        if (alreadyGuessedArray.length === 0) {
+            wordGame.pushAlreadyGuessed(userKey);
+            wordGame.findMatch(userKey);
+        } else if (alreadyGuessedArray.indexOf(userKey) === -1) {
+            wordGame.pushAlreadyGuessed(userKey);
+            wordGame.findMatch(userKey);
+        }
+        // wordGame.findMatch(userKey);
+    }
+});
+
+
 // Get Player Input from Document
-document.onkeypress = function(event) {
+document.onkeypress = function (event) {
     userKey = event.key.toLowerCase();
     console.log(event.keyCode); // displays a coe number
     console.log(event.key); // displays what key was pressed
